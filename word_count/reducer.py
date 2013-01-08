ad8d05e6eb093f25447d01ab3602dc05f019f076
cgopalan <cgopalan@Chandrakant-Gopalans-MacBook.local>
2013-01-08 17:45:46 -0500

    Initial commit

---
+++ word_count/reducer.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python3
+
+import sys
+from collections import Counter
+
+
+def run_reduce(f):
+    cnt = Counter()
+    for line in f:
+        data = line.rstrip()
+        cnt[data] += 1
+    for word, count in cnt.items():
+        print(word,':',count)
+
+
+if __name__ == '__main__':
+    run_reduce(sys.stdin)
